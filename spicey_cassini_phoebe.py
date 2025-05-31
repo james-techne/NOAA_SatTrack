5ead0e1e4600d9c0be1938641fc311e5486da829
James Phillips <japh7832@colorado.edu>
2025-05-31 11:09:41 -0600

    Completed most of the lesson. Important information is contained in the SPK files. Need to be able to translate TLEs to SPKs in order to make use of the SPICE functions.

--- spicey_cassini_phoebe.py
+++ spicey_cassini_phoebe.py
@@ -5,4 +5,21 @@ from builtins import input
 import numpy as np
 
 
-METAKR = "./metakr_remotesensing.txt"
+METAKR = "./metakr_cassiniphoebe.txt"
+spice.furnsh(METAKR)
+
+utctim = input('Please input a time: ')
+print('Converting time string {:s}'.format(utctim))
+
+et = spice.str2et(utctim)
+print('Ephemeris Time is: {:16.3f}'.format(et))
+
+[state, ltime] = spice.spkezr('Phoebe', et, 'J2000','LT+S',
+                              'Cassini')
+
+print('     X = {:16.3f}'.format(state[0]))
+print('     Y = {:16.3f}'.format(state[1]))
+print('     Z = {:16.3f}'.format(state[2]))
+print('     VX = {:16.3f}'.format(state[3]))
+print('     VY = {:16.3f}'.format(state[4]))
+print('     VZ = {:16.3f}'.format(state[5]))
